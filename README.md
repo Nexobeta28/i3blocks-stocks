da5082966422e29a5d361fb7e418badacf4d59e3
Nexobeta28 <alvaroesparza2@gmail.com>
2020-09-10 23:34:58 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# i3blocks-stocks
+An [i3blocks](https://github.com/vivien/i3blocks) implementation for stock visualizing.
+
---
+++ stocks/get-stock.py
@@ -0,0 +1,26 @@
+# constants
+SYMBOL = "TSLA"
+API_KEY = "BC57Z7PKR6DSYH8D"
+
+# imports
+import urllib, json
+from datetime import datetime, timedelta
+
+# get data
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s' % (SYMBOL, API_KEY)
+response = urllib.urlopen(url)
+
+# get date
+yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+dayBeforeYesterday = (datetime.today() - timedelta(days=2)).strftime('%Y-%m-%d')
+
+#calculate
+data = json.loads(response.read())['Time Series (Daily)']
+
+yesterdayData = float(data[yesterday]['4. close'])
+dayBeforeYesterdayData = float(data[dayBeforeYesterday]['4. close'])
+
+percentage = round(((100 * yesterdayData) / dayBeforeYesterdayData) - 100, 2)
+
+# print
+print(SYMBOL + " (daily)" + ": " + str(percentage) + "%")
